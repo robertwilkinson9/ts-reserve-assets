94e1e7a97661ef7af7776f83d9eaf1d2e45e7a88
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-05-11 16:54:29 +0100

    start to lift up state to top

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { useState } from "react";
 */ }
+import { useState } from "react";
 
 import { Header } from './components/header'
 import { Calendar } from './components/calendar'
@@ -9,19 +10,24 @@ import { Desks } from './components/desks'
 import { AddEmail } from './components/addemail'
 
 export const App = () => {
-//   const [startDate, setStartDate] = useState<Date|null>(new Date());
+   const [startDate, setStartDate] = useState<Date|null>(new Date());
+   console.log("startDate starts at ");
+   console.log(startDate.toLocaleDateString());
 
    return (
      <>
      <Header />
-     <Calendar />
 { /*
-//     console.log("startDate starts at ");
-//    console.log(startDate.toLocaleDateString());
-//     console.log(`startDate starts at {startDate}`
-//     <Calendar start={startDate} setter={setStartDate} />
-//     console.log("startDate is now ");
-//     console.log(startDate.toLocaleDateString());
+     <Calendar />
+
+    console.log("startDate starts at ");
+      console.log(startDate.toLocaleDateString());
+     console.log(`startDate starts at {startDate}`
+*/ }
+     <Calendar start={startDate} setter={setStartDate} />
+{ /*
+     console.log("startDate is now ");
+     console.log(startDate.toLocaleDateString());
 */ }
      <Desks />
      <AddEmail />
--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,8 +1,8 @@
 { /*
 import React, { useState } from "react";
+import { useState } from "react";
 */ }
 
-import { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -10,10 +10,18 @@ import "react-datepicker/dist/react-datepicker.css";
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-export const Calendar = () => {
-  const [startDate, setStartDate] = useState<Date|null>(new Date());
+export interface CalendarProps {
+   start: Date | null;
+   setter: any;
+}
+
+export const Calendar = (props: CalendarProps) => {
+//  const [startDate, setStartDate] = useState<Date|null>(new Date());
   return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    <>
+    <h4> Calendar </h4>
+    <DatePicker selected={props.start} onChange={(date) => props.setter(date)} />
+    </>
   );
 };
 
