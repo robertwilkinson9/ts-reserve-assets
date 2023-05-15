67b8a3ef3f88c7000e347ffe1aff2c0a4c06dc81
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-05-15 12:06:51 +0100

    finish tidy of calendar

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,12 +1,4 @@
-{ /*
-import React, { useState } from "react";
-import { useState } from "react";
-*/ }
-
 import DatePicker from "react-datepicker";
-
-import { registerLocale, getDefaultLocale, setDefaultLocale } from  "react-datepicker";
-import enGB from 'date-fns/locale/en-GB';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -15,22 +7,10 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export interface CalendarProps {
    start: Date | null;
-   setter: any;
+   setter: React.Dispatch<React.SetStateAction<Date | null>>
 }
 
 export const Calendar = (props: CalendarProps) => {
-//  const [startDate, setStartDate] = useState<Date|null>(new Date());
-//  console.log("startDate starts at ");
-//  console.log(startDate.toLocaleDateString());
-
-registerLocale('en-GB', enGB)
-
-console.log("BEFORE ... ");
-console.log(getDefaultLocale);
-
-setDefaultLocale('en-GB');
-console.log("FTER ... ");
-console.log(getDefaultLocale);
  return (
     <>
     <h4> Calendar </h4>
