d82716536ee03d71e2b9be345c6e95f9e0612a15
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-05-18 15:03:13 +0100

    take form out of addemail - we have a form component and do not need nested forms

--- src/components/addemail.tsx
+++ src/components/addemail.tsx
@@ -1,74 +1,49 @@
-import { useState } from "react";
-
-// useForm functional component
-export const useForm = (callback: any, initialState = {}) => {
-    const [values, setValues] = useState(initialState);
-
-    // onChange
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValues({ ...values, [event.target.name]: event.target.value });
-    };
-
-
-    // onSubmit
-    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(event);
-        await callback(); // triggering the callback
-    };
-
-    // return values
-    return {
-        onChange,
-        onSubmit,
-        values,
-    };
-}
-
 export interface AddEmailProps {
-  setter: React.Dispatch<React.SetStateAction<string | null>>;
+  email: string | null;
+  emailsetter: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-export const AddEmail = ({setter} : AddEmailProps) =>{
-    console.log("SETTER is")
-    console.log(setter)
+export const AddEmail = ({email, emailsetter} : AddEmailProps) =>{
+  console.log("EMAILSETTER is")
+  console.log(emailsetter)
 
- // defining the initial state for the form
-    const initialState = {
-        email: "",
-        password: "",
-    };
+{ /*
+  const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
 
-    // getting the event handlers from our custom hook
-    const { onChange, onSubmit, values } = useForm(
-        loginUserCallback,
-        initialState
-    );
+    console.log("PRINT event.target ");
+    console.log("event -> ", event);
+    console.log("event.currentTarget -> ", event.currentTarget);
+    const submit_value = (event.currentTarget.elements[3] as HTMLInputElement).value; // 4th element we want the value of the submit button
+    console.log("SUBMIT_VALUE -> ", submit_value);
+//    console.log(`EMAIL you entered was: ${email}`)
+//    alert("EMAIL you entered")
+  }
+*/ }
 
-    console.log("Values from form are ");
-    console.log(values);
+  if (typeof(email) !== "string") {email = "";}
 
-    // a submit function that will execute upon form submission
-    async function loginUserCallback() {
-        // send "values" to database
-    }
-
-    return (
-        // don't mind this ugly form :P
-        <form onSubmit={onSubmit}>
-        <div>
-            <input
-                name='email'
-                id='email'
-                type='email'
-                placeholder='Email'
-                onChange={onChange}
-                required
-                />
-            <button type='submit'>Book Desk</button>
-        </div>
-        </form>
-    );
+  return (
+    <>
+{ /*
+    <form onSubmit={handleSubmit} id="emailForm">
+*/ }
+      <label>Enter your email:
+        <input 
+          type="text"
+          value={email}
+          onChange={(e) => emailsetter(e.target.value)}
+        />
+      </label>
+{ /*
+      <button type="submit" value="submit" form="emailForm" >Book Desk</button>
+    </form>
+*/ }
+    </>
+  );
 }
 
 export default AddEmail;
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -9,25 +9,43 @@ import { Floor } from './floor'
 import { Desks } from './desks'
 import { AddEmail } from './addemail'
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    console.log("PRINT event.target ");
+    console.log("event -> ", event);
+    console.log("event.currentTarget -> ", event.currentTarget);
+    const submit_value = (event.currentTarget.elements[3] as HTMLInputElement).value; // 4th element we want the value of the submit button
+    console.log("SUBMIT_VALUE -> ", submit_value);
+//    console.log(`EMAIL you entered was: ${email}`)
+//    alert("EMAIL you entered")
+  }
+
 export interface InputFormProps {
   start: Date | null;
   datesetter: React.Dispatch<React.SetStateAction<Date | null>>;
   floor: number | null;
   floorsetter: React.Dispatch<React.SetStateAction<number | null>>;
   desksetter: React.Dispatch<React.SetStateAction<string | null>>;
+  email: string | null;
   emailsetter: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-export const InputForm = ({start, datesetter, floor, floorsetter, desksetter, emailsetter}: InputFormProps) => {
+export const InputForm = ({start, datesetter, floor, floorsetter, desksetter, email, emailsetter}: InputFormProps) => {
   return (
-    <Form>
+    <>
+{ /*
+     <Form id="emailForm">
+*/ }
+     <Form onSubmit={handleSubmit} id="emailForm">
      <Calendar start={start} setter={datesetter} />
 { /*
      <Testapp />
 */ }
      <Floor floor={floor} floorsetter={floorsetter}/>
      <Desks floor={floor} desksetter={desksetter} />
-     <AddEmail setter={emailsetter} />
+     <AddEmail email={email} emailsetter={emailsetter} />
+     <button type="submit" value="submit" form="emailForm" onClick={() => {console.log("AARGGHH")}} >Book Desk</button>
 { /*
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
@@ -49,6 +67,7 @@ export const InputForm = ({start, datesetter, floor, floorsetter, desksetter, em
       </Button>
 */ }
     </Form>
+    </>
   );
 }
 
