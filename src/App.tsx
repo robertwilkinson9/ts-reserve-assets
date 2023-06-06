cd7fac1cb0c229fbf7a3b8ac3432082d8e12cde9
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-06-06 09:04:12 +0100

    process data in the process data file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 
-import axios from 'axios'
-
 import { registerLocale, setDefaultLocale } from  "react-datepicker";
 import enGB from 'date-fns/locale/en-GB';
 
@@ -26,7 +24,6 @@ const reset = (
   setEmail(null);
   setComplete(false);
 }
-*/ }
 
 interface DeskData {
   "booking_start": string;
@@ -37,23 +34,9 @@ interface DeskData {
   "email": string
 }
 
+*/ }
+
 type string_or_null = string | null;
-
-const tomorrow_from_day = (startDateTime: Date): Date => {
-  // Current date
-  const date = new Date(startDateTime!);
-  // Tomorrow's date
-  const tomorrow = date.setDate(date.getDate() + 1);
-
-  return new Date(tomorrow);
-}
-
-const add_desk_to_mongodb = async (url: string, desk_booking: DeskData) => {
-  console.log("ADD_DESK_TO_MONGODB and BEFORE desk_booking is ", JSON.stringify(desk_booking));
-  const response = await axios.post(url, desk_booking);
-  console.log('STATUS:', response.status)
-  console.log('DATA IS :', response.data)
-};
 
 export const App = () => {
   const [startDateTime, setStartDateTime] = useState<Date|null>(new Date());
@@ -77,7 +60,6 @@ export const App = () => {
   const [desk, setDesk] = useState<string|null>(null);
   const [email, setEmail] = useState<string|null>(null);
   const [complete, setComplete] = useState<boolean>(false);
-  const [datasent, setDatasent] = useState<boolean>(false);
   const [mongodesks, setMongodesks] = useState<string_or_null[]>([null]);
 
   registerLocale('en-GB', enGB)
@@ -107,32 +89,11 @@ export const App = () => {
   console.log("after useEffect App and DESKS are ", JSON.stringify(mongodesks));
 
   if (complete) {
-    console.log("Form is complete - we can SUBMIT IT XXX");
-    console.log(`StartDateTime is ${startDateTime}, EndDateTime is ${endDateTime}, Floor is ${floor}, Desk is ${desk}, Email is ${email}`);
-    
-    const tomorrow = tomorrow_from_day(startDateTime!);
-    console.log("tomorrow is ", tomorrow);
-
-    const desk_booking = {
-      "booking_start": startDateTime!.toString(),
-      "booking_end": endDateTime!.toString(),
-      "expireAt": tomorrow.toString(),
-      "floor": floor!,
-      "desk": desk!,
-      "email": email!,
-    };
-    if (!datasent) {
-      add_desk_to_mongodb(DESK_url, desk_booking);
-      console.log(`0. SET_DATASENT - DATASENT IS ${datasent}`)
-      setDatasent(true);
-      console.log(`1. SET_DATASENT - DATASENT IS ${datasent}`)
-    }
-
 //    reset(setStartDateTime, setEndDateTime, setFloor, setDesk, setEmail, setComplete); // eventually just show processdata screen for now
     return (
       <>
       <Header />
-      <ProcessData start={startDateTime} end={endDateTime} floor={floor} desk={desk} email={email} />
+      <ProcessData start={startDateTime} end={endDateTime} floor={floor} desk={desk} email={email} url={DESK_url} />
       <InputForm start={startDateTime} startdatesetter={setStartDateTime} end={endDateTime} enddatesetter={setEndDateTime} floor={floor} floorsetter={setFloor} desksetter={setDesk} email={email} emailsetter={setEmail} completesetter={setComplete} />
       </>
     );
--- src/components/processdata.tsx
+++ src/components/processdata.tsx
@@ -1,14 +1,62 @@
+import { useState } from "react";
+import axios from 'axios'
+
 export interface ProcessDataProps {
    start: Date | null;
    end: Date | null;
    floor: number | null;
    desk: string | null;
    email: string | null;
+   url: string;
 }
 
-const Floor = ["Ground", "First", "Second"];
- 
-export const ProcessData = ({ start, end, floor, desk, email } : ProcessDataProps) => {
+const tomorrow_from_day = (startDateTime: Date): Date => {
+  // Current date
+  const date = new Date(startDateTime!);
+  // Tomorrow's date
+  const tomorrow = date.setDate(date.getDate() + 1);
+
+  return new Date(tomorrow);
+}
+
+interface DeskData {
+  "booking_start": string;
+  "booking_end": string;
+  "expireAt": string;
+  "floor": number;
+  "desk": string;
+  "email": string
+}
+
+const add_desk_to_mongodb = async (url: string, desk_booking: DeskData) => {
+  console.log("ADD_DESK_TO_MONGODB and BEFORE desk_booking is ", JSON.stringify(desk_booking));
+  const response = await axios.post(url, desk_booking);
+  console.log('STATUS:', response.status)
+  console.log('DATA IS :', response.data)
+};
+
+export const ProcessData = ({ start, end, floor, desk, email, url } : ProcessDataProps) => {
+  const [datasent, setDatasent] = useState<boolean>(false);
+
+  const tomorrow = tomorrow_from_day(start!);
+  console.log("tomorrow is ", tomorrow);
+
+  const desk_booking = {
+    "booking_start": start!.toString(),
+    "booking_end": end!.toString(),
+    "expireAt": tomorrow.toString(),
+    "floor": floor!,
+    "desk": desk!,
+    "email": email!,
+  };
+
+  if (!datasent) {
+    add_desk_to_mongodb(url, desk_booking);
+    console.log(`0. SET_DATASENT - DATASENT IS ${datasent}`)
+    setDatasent(true);
+    console.log(`1. SET_DATASENT - DATASENT IS ${datasent}`)
+  }
+
   let sdstr = "No start date available"
   if (start) {
     sdstr = `start date is ${start}`;
@@ -17,13 +65,15 @@ export const ProcessData = ({ start, end, floor, desk, email } : ProcessDataProp
   if (end) {
     edstr = `end date is ${end}`;
   }
+  const Floor = ["Ground", "First", "Second"];
   let fstr = "No Floor";
   if (floor === 0 || floor === 1 || floor === 2) {
     fstr = Floor[floor] + " floor";
   }
+
   return (
     <>
-    <h4> ProcessData </h4>
+    <h4>ProcessData</h4>
     <p>{sdstr}</p>
     <p>{edstr}</p>
     <p>{fstr}</p>
