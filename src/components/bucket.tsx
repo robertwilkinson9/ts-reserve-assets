3735aa1dac9ae13d60062662feb91d1ae64f63f4
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-06-13 12:13:12 +0100

    start to compute button values from conggig data

--- src/components/bucket.tsx
+++ src/components/bucket.tsx
@@ -1,6 +1,6 @@
 import './bucket.css';
 
-import { BucketProps, ButtonProps } from './interfaces';
+import { BucketProps, ButtonProps, configData } from './interfaces';
 
 const BucketLabel = () => {
   return (
@@ -40,7 +40,6 @@ const BucketButton = ({cb, lcf, ucf, bucketst, checked} : ButtonProps) => {
   }
 }
 
-function capitalizeFirstLetter(name: string) {return name.charAt(0).toUpperCase() + name.slice(1);}
 
 export const Bucket = ({config, bucket, bucketsetter}: BucketProps) => {
   console.log(`CONFIG.BUCKET_NAME is ${config.BUCKET_NAME}`);
@@ -52,20 +51,74 @@ export const Bucket = ({config, bucket, bucketsetter}: BucketProps) => {
     bucketsetter(parseInt(e.target.value, 10));
   }
 
+  const capitalizeFirstLetter = (name: string) => {return name.charAt(0).toUpperCase() + name.slice(1);}
+
+{ /*
+
   for (let i = 0; i < config.BUCKETS.length; i++) {
     const name = config.BUCKETS[i].name;
     console.log(`BUCKET i name is ${name} OR ${capitalizeFirstLetter(name)}`);
   }
+*/}
+
+  const build_config_matrix = (config: configData)  => {
+    const matrix = [];
+    for (let i = 0; i < config.BUCKETS.length; i++) {
+      const name = config.BUCKETS[i].name;
+      const uname = capitalizeFirstLetter(name);
+      const vector = [ name, uname, i.toString()];
+      console.log("VECTOR is ");
+      console.log(vector);
+      matrix.push(vector);
+   }
+   console.log("MATRIX is ");
+   console.log(matrix);
+   return matrix;
+ }
+
+  const build_checked_vector = (config: configData, bucket: number | null)  => {
+    const checked = []
+    for (let i = 0; i < config.BUCKETS.length; i++) {
+      const set = i == bucket;
+      checked.push(set);
+   }
+   console.log("CHECKED is ");
+   console.log(checked);
+   return checked;
+ }
 
   switch (bucket)  {
     case 0:
+      const matrix = build_config_matrix(config);
+      const checked = build_checked_vector(config, bucket);
+      console.log("2. MATRIX is ");
+      console.log(matrix);
+      console.log("2 CHECKED is ");
+      console.log(checked);
+      console.log("0.0 MATRIX is ");
+      console.log(matrix[0][0]);
+      console.log("0.1 MATRIX is ");
+      console.log(matrix[0][2]);
+      console.log("0.2 MATRIX is ");
+      console.log(matrix[0][2]);
+      console.log("0 CHECKED is ");
+      console.log(checked[0]);
         return(
         <>
           <BucketLabel />
           <div className="container">
+            <BucketButton cb={handleChange} lcf={matrix[0][0]} ucf={matrix[0][1]} bucketst={matrix[0][2]} checked={checked[0]} />
+            <BucketButton cb={handleChange} lcf={matrix[1][0]} ucf={matrix[1][1]} bucketst={matrix[1][2]} checked={checked[1]} />
+            <BucketButton cb={handleChange} lcf={matrix[2][0]} ucf={matrix[2][1]} bucketst={matrix[2][2]} checked={checked[2]} />
+{ /*
+
+            <BucketButton cb={handleChange} lcf={matrix[0][0]} ucf={matrix[0][1]} bucketst={matrix[0][2]} checked={matrix[0][3]} />
+            <BucketButton cb={handleChange} lcf={matrix[0][1]} ucf={matrix[1][1]} bucketst={matrix[1][2]} checked={matrix[1][3]} />
+            <BucketButton cb={handleChange} lcf={matrix[0][2]} ucf={matrix[2][1]} bucketst={matrix[2][2]} checked={matrix[2][3]} />
             <BucketButton cb={handleChange} lcf="ground" ucf="Ground" bucketst="0" checked={true} />
             <BucketButton cb={handleChange} lcf="first" ucf="First" bucketst="1" checked={false} />
             <BucketButton cb={handleChange} lcf="second" ucf="Second" bucketst="2" checked={false} />
+*/ }
           </div>
         </>)
     case 1:
