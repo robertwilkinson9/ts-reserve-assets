7affbf25c9090161fe84917615a63e92bf8231d7
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-09-01 13:12:43 +0100

    add an enclosing Box

--- src/components/addemail.tsx
+++ src/components/addemail.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormLabel, FormHelperText, Input} from '@chakra-ui/react'
+import { Box, FormControl, FormLabel, FormHelperText, Input} from '@chakra-ui/react'
 
 import { AddEmailProps } from './interfaces';
 
@@ -7,18 +7,22 @@ export const AddEmail = ({email, set_email} : AddEmailProps) =>{
 
   return (
     <>
+      <Box>
       <div data-testid="emailaddress" id="emailaddress">
         <FormControl>
           <FormLabel data-testid='emailaddress_label' htmlFor='email' >Email address</FormLabel>
 { /*
+          <Input id='email' type='email' value="Email address"  onChange={(event) => set_email(event.target.value)} />;
           <Input id='email' type='email' value=email ? {email} : "Email address"  onChange={(event) => set_email(event.target.value)} />;
-*/ }
           email ? 
             <Input id='email' type='email' value={email} onChange={(event) => set_email(event.target.value)} /> :
             <Input id='email' type='email' value="Email address" onChange={(event) => set_email(event.target.value)} />;
+*/ }
+          <Input id='email' type='email' value={email} onChange={(event) => set_email(event.target.value)} />
           <FormHelperText>We'll never share your email.</FormHelperText>
         </FormControl>
       </div>
+      </Box>
     </>
   );
 }
