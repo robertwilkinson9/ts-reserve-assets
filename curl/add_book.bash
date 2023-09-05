3e21d6679af57aac710107dd5b5648accee664b7
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-09-05 17:12:03 +0100

    use ssl

--- curl/add_book.bash
+++ curl/add_book.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -X POST -H "Content-Type: application/json" --data @new.book http://192.168.1.185:5180/api/book/
+#curl -X POST -H "Content-Type: application/json" --data @new.book http://192.168.1.185:5180/api/book/
+curl -X POST -H "Content-Type: application/json" --data @new.book --insecure https://localhost:443/api/book/
