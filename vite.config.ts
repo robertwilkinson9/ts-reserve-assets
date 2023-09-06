16d4f4ad4b1c6daae479e8534b35973dcb3732c9
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-09-06 10:13:10 +0100

    get https to work using mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   define: {'process.argv': process.argv}, 
+  server: {
+    https: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
@@ -12,6 +18,10 @@ export default defineConfig({
     setupFiles: './setup.js',
   }
 });
+//    options: {
+//      key: fs.readFileSync("/home/robert/src/typescript/ts-REST-api-for-mongodb/certs/localhost.key"),
+//      crt: fs.readFileSync("/home/robert/src/typescript/ts-REST-api-for-mongodb/certs/localhost.crt")
+//    },
 //    environment: 'happy-dom',
 //    reporter: [ "json", "html"],
 //    reporter: [ "default", "dot"],
