e9a1400a3c9d3af26ab3f14fc1ef6df8d9af7c53
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-09-11 14:53:39 +0100

    move from vite-plugin-mkcert to @vitejs/plugin-basic-ssl as suggested in vite documentation

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), basicSsl()],
   define: {'process.argv': process.argv}, 
   server: {
     https: true,
@@ -18,11 +18,3 @@ export default defineConfig({
     setupFiles: './setup.js',
   }
 });
-//    options: {
-//      key: fs.readFileSync("/home/robert/src/typescript/ts-REST-api-for-mongodb/certs/localhost.key"),
-//      crt: fs.readFileSync("/home/robert/src/typescript/ts-REST-api-for-mongodb/certs/localhost.crt")
-//    },
-//    environment: 'happy-dom',
-//    reporter: [ "json", "html"],
-//    reporter: [ "default", "dot"],
-//    reporter: [ "dot"],
