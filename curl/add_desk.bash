f4541345eec086a75f2c5583f4d3b06b253ea760
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-10-18 12:56:39 +0100

    add desk to localhost mongo script

--- curl/add_desk.bash
+++ curl/add_desk.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 #curl -X POST -H "Content-Type: application/json" --data @new.item http://10.0.2.15:5179/api/item/
-curl -X POST -H "Content-Type: application/json" --data @new.desk --insecure https://localhost:5179/api/desk/
+#curl -X POST -H "Content-Type: application/json" --data @new.desk --insecure https://localhost:5179/api/desk/
+#curl -X POST -H "Content-Type: application/json" --data @new.desk http://192.168.1.185:5180/api/desk/
+#curl -X POST -H "Content-Type: application/json" --data @new.desk --insecure https://localhost:443/api/desk/
+IPA=localhost
+IPP=$(grep APIPORT ../config/config.desk.json | awk '{print $NF}' | sed -e 's/,$//')
+curl -X POST -H "Content-Type: application/json" --data @new.desk --insecure https://${IPA}:${IPP}/api/desk/
