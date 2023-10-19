28ab0b5a8dad87d5f96a5be1f21e4f5cbe07f427
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-10-19 15:48:43 +0100

    use API_IP for VITE_API_IP, whatever that was

--- make_type_yaml.sh
+++ make_type_yaml.sh
@@ -19,7 +19,6 @@ services:
       SSL_CERT: "/certs/localhost.crt"
       SSL_KEY: "/certs/localhost.key"
       API_IP: ${API_IP}
-      VITE_API_IP: ${API_IP}
       API_PORT: ${API_PORT} 
       TYPE: ${TYPE}
 EOF
