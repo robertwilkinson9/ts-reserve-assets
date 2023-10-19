2dfe40174e497b94e01588de65bfe4ae3ebd91dd
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-10-19 17:33:23 +0100

    add quotes for VITE env variables and start to write a Dockerfile but no use now

--- make_type_yaml.sh
+++ make_type_yaml.sh
@@ -18,7 +18,41 @@ services:
     environment:
       SSL_CERT: "/certs/localhost.crt"
       SSL_KEY: "/certs/localhost.key"
-      VITE_API_IP: ${API_IP}
-      VITE_API_PORT: ${API_PORT} 
-      VITE_TYPE: ${TYPE}
+      VITE_API_IP: "${API_IP}"
+      VITE_API_PORT: "${API_PORT}"
+      VITE_TYPE: "${TYPE}"
 EOF
+
+cat <<EOF2 > Dockerfile
+FROM node:latest
+
+ARG SSL_CERT
+ENV SSL_CERT ${SSL_CERT}
+ARG SSL_KEY
+ENV SSL_KEY ${SSL_KEY}
+ARG API_IP
+ENV API_IP ${API_IP}
+ARG API_PORT
+ENV API_PORT ${API_PORT}
+ARG TYPE
+ENV TYPE ${TYPE}
+
+RUN apt update
+RUN apt install -y git npm
+RUN mkdir /certs
+ADD ./certs/ /certs
+RUN mkdir /src 
+RUN git clone https://github.com/robertwilkinson9/ts-reserve-assets.git /src/ts-reserve-assets
+WORKDIR /src/ts-reserve-assets
+RUN npm install
+
+#ENV NODE_VERSION 15.8.0
+#RUN apt update\
+#    && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash\
+#    && . $HOME/.nvm/nvm.sh\
+#    && nvm install $NODE_VERSION
+#ENV NODE_PATH /root/.nvm/v$NODE_VERSION/lib/node_modules
+#ENV PATH /root/.nvm/versions/node/v$NODE_VERSION/bin:$PATH
+
+CMD [ "npm", "run", ${TYPE} ]
+EOF2
