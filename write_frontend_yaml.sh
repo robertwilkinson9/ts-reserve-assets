545b0b2cebed0bbad679ba086ba0ed21425038c2
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-11-06 10:46:57 +0000

    this writes desk frontend file on kubernetes in appropriate directory - SRC_DIR!

--- write_frontend_yaml.sh
+++ write_frontend_yaml.sh
@@ -1,9 +1,19 @@
 #/bin/bash
 TYPE=$1
-BACKEND_IP=$(./get_backend_IP ${TYPE})
-echo beip is $BACKEND_IP
-BACKEND_PORT=$(kubectl describe pod/${TYPE}-backend | grep API_PORT | awk '{print $NF}')
-echo beport is $BACKEND_PORT
+PORT=$2
+#BACKEND_IP=$(kubectl describe service/${TYPE}-backend-service | grep ^IP: | awk '{print $NF}')
+#echo beip is $BACKEND_IP
+#BACKEND_PORT=$(kubectl describe service/${TYPE}-backend-service | grep TargetPort: | awk '{print $2}' | awk -F/ '{print $1}')
+#echo beport is $BACKEND_PORT
+SRC_DIR="../../typescript/ts-reserve-assets/"
+FE_PORT=$(cat ${SRC_DIR}/package.json | jq --raw-output .config.${TYPE})
+echo FE_PORT is $FE_PORT
+END_POINTS=$(kubectl describe service/${TYPE}-backend-service | grep ^Endpoints: | awk '{print $NF}')
+echo endpoints is $END_POINTS
+END_POINT_IP=$(echo ${END_POINTS} | awk -F: '{print $1}')
+echo endpoint_ip is $END_POINT_IP
+END_POINT_PORT=$(echo ${END_POINTS} | awk -F: '{print $2}')
+echo endpoint_port is $END_POINT_PORT
 
 cat << EOF > ${TYPE}-frontend.yaml
 ---
@@ -18,16 +28,16 @@ spec:
     - name: ${TYPE}-frontend
       image: robertwilkinsonwork299/reserve-assets-${TYPE}-frontend
       ports:
-        - containerPort: 5176
+        - containerPort: ${FE_PORT}
       env:
       - name: SSL_CERT
         value: /certs/localhost.crt
       - name: SSL_KEY
         value: /certs/localhost.key
       - name: VITE_API_IP
-        value: "${BACKEND_IP}"
+        value: "${END_POINT_IP}"
       - name: VITE_API_PORT
-        value: "${BACKEND_PORT} "
+        value: "${END_POINT_PORT}"
       - name: VITE_TYPE
         value: "${TYPE}"
 EOF
