07c0d659a16cb93b04839af9b38611708c28c7f3
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-03-01 12:22:42 +0000

    use screen to get the rendered data

--- src/__tests__/form.test.tsx
+++ src/__tests__/form.test.tsx
@@ -3,7 +3,7 @@
  */
 
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 // import Form from 'react-bootstrap/Form';
 
@@ -95,10 +95,6 @@ describe('overlap function tests', () => {
     const ydate = new Date("1999-12-31T02:00");
 
     const overlap_result = overlap(adate, bdate, xdate, ydate);
-/*
-    console.log(overlap_result);
-    console.dir(overlap_result);
-*/
     expect(overlap_result).toBe(true);
   });
 })
@@ -111,87 +107,67 @@ describe('it item contents', () => {
 
     expect(InputFormlabel).toHaveTextContent('Email address');
 
-//   const InputForm = await findByTestId("emailaddress");
-//   expect(InputForm).toContainElement(InputFormlabel);
+   const InputForm = screen.queryByTestId("emailaddress");
+   expect(InputForm).toContainElement(InputFormlabel);
   });
 
   it("form should contain all parts", async () => {
-//grep testid form.html 
     const email = "email@domain.org";
     const itemprops = {email: email};
 
-//    const { findByTestId, queryAllByTestId } = renderInputForm(itemprops);
+    const { findByTestId, queryAllByTestId } = renderInputForm(itemprops);
 
-////        data-testid="calendar_label"
-////        data-testid="calendar_datepicker"
-////        data-testid="calendar_label"
-////        data-testid="calendar_datepicker"
-//
-//    const calendar_label_0 = await queryAllByTestId("calendar_label")[0];
-//    expect(calendar_label_0).toBeInTheDocument();
-//
-//    const calendar_datepicker_0 = await queryAllByTestId("calendar_datepicker")[0];
-//    expect(calendar_datepicker_0).toBeInTheDocument();
-//
-//    const calendar_label_1 = await queryAllByTestId("calendar_label")[1];
-//    expect(calendar_label_1).toBeInTheDocument();
-//
-//    const calendar_datepicker_1 = await queryAllByTestId("calendar_datepicker")[1];
-//    expect(calendar_datepicker_1).toBeInTheDocument();
-//
-////        data-testid="bucket_label_div"
-////            data-testid="bucket_label_label"
-////        data-testid="bucket_div"
-//
-//    const bucket_label_div = await findByTestId("bucket_label_div");
-//    expect(bucket_label_div).toBeInTheDocument();
-//
-//    const bucket_label_label = await findByTestId("bucket_label_label");
-//    expect(bucket_label_label).toBeInTheDocument();
-//
-//    const bucket_div = await findByTestId("bucket_div");
-//    expect(bucket_div).toBeInTheDocument();
-//
-////          data-testid="bucket_button_checked"
-////              data-testid="bucket_button_checked_input"
-////              data-testid="bucket_button_checked_label"
-////          data-testid="bucket_button_unchecked"
-////              data-testid="bucket_button_unchecked_input"
-////              data-testid="bucket_button_unchecked_label"
-//
-//    const bucket_button_checked = await findByTestId("bucket_button_checked");
-//    expect(bucket_button_checked).toBeInTheDocument();
-//
-//    const bucket_button_checked_input = await findByTestId("bucket_button_checked_input");
-//    expect(bucket_button_checked_input).toBeInTheDocument();
-//
-//    const bucket_button_checked_label = await findByTestId("bucket_button_checked_label");
-//    expect(bucket_button_checked_label).toBeInTheDocument();
-//
-//    const bucket_button_unchecked = await findByTestId("bucket_button_unchecked");
-//    expect(bucket_button_unchecked).toBeInTheDocument();
-//
-//    const bucket_button_unchecked_input = await findByTestId("bucket_button_unchecked_input");
-//    expect(bucket_button_unchecked_input).toBeInTheDocument();
-//
-//    const bucket_button_unchecked_label = await findByTestId("bucket_button_unchecked_label");
-//    expect(bucket_button_unchecked_label).toBeInTheDocument();
-//
-////        data-testid="items_div"
-////          data-testid="items_label"
-////        data-testid="emailaddress"
-////            data-testid="emailaddress_label"
-//
-//    const items_div = await findByTestId("items_div");
-//    expect(items_div).toBeInTheDocument();
-//
-//    const items_label = await findByTestId("items_label");
-//    expect(items_label).toBeInTheDocument();
-//
-//    const EmailForm = await findByTestId("emailaddress");
-//    expect(EmailForm).toBeInTheDocument();
-//
-//    const EmailFormlabel = await findByTestId("emailaddress_label");
-//    expect(EmailFormlabel).toBeInTheDocument();
+    const calendar_label_0 = screen.queryAllByTestId("calendar_label")[0];
+    expect(calendar_label_0).toBeInTheDocument();
+
+    const calendar_datepicker_0 = screen.queryAllByTestId("calendar_datepicker")[0];
+    expect(calendar_datepicker_0).toBeInTheDocument();
+
+    const calendar_label_1 = screen.queryAllByTestId("calendar_label")[1];
+    expect(calendar_label_1).toBeInTheDocument();
+
+    const calendar_datepicker_1 = screen.queryAllByTestId("calendar_datepicker")[1];
+    expect(calendar_datepicker_1).toBeInTheDocument();
+
+    const bucket_label_div = screen.queryByTestId("bucket_label_div");
+    expect(bucket_label_div).toBeInTheDocument();
+
+    const bucket_label_label = screen.queryByTestId("bucket_label_label");
+    expect(bucket_label_label).toBeInTheDocument();
+
+    const bucket_div = screen.queryByTestId("bucket_div");
+    expect(bucket_div).toBeInTheDocument();
+
+    const bucket_button_checked = screen.queryByTestId("bucket_button_checked");
+    expect(bucket_button_checked).toBeInTheDocument();
+
+    const bucket_button_checked_input = screen.queryByTestId("bucket_button_checked_input");
+    expect(bucket_button_checked_input).toBeInTheDocument();
+
+    const bucket_button_checked_label = screen.queryByTestId("bucket_button_checked_label");
+    expect(bucket_button_checked_label).toBeInTheDocument();
+
+    const bucket_button_unchecked = screen.queryByTestId("bucket_button_unchecked");
+    expect(bucket_button_unchecked).toBeInTheDocument();
+
+    const bucket_button_unchecked_input = screen.queryByTestId("bucket_button_unchecked_input");
+    expect(bucket_button_unchecked_input).toBeInTheDocument();
+
+    const bucket_button_unchecked_label = screen.queryByTestId("bucket_button_unchecked_label");
+    expect(bucket_button_unchecked_label).toBeInTheDocument();
+
+    const items_div = screen.queryByTestId("items_div");
+    expect(items_div).toBeInTheDocument();
+
+    const items_label = screen.queryByTestId("items_label");
+    expect(items_label).toBeInTheDocument();
+
+    const EmailForm = screen.queryByTestId("emailaddress");
+    expect(EmailForm).toBeInTheDocument();
+
+    const EmailFormlabel = screen.queryByTestId("emailaddress_label");
+    expect(EmailFormlabel).toBeInTheDocument();
+
+//    screen.debug();
   });
 });
