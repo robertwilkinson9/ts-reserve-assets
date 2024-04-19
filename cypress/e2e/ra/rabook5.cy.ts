58a3a28ffaad4e133d3007a816160f5963312694
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-04-19 10:04:34 +0100

    code tidy

--- cypress/e2e/ra/rabook5.cy.ts
+++ cypress/e2e/ra/rabook5.cy.ts
@@ -3,12 +3,7 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('rabook app', () => {
-/*
   beforeEach(() => {
-  })
-*/
-
-  it('Reserve "Northanger Abbey"', () => {
     cy.visit('https://10.0.2.15:5176/');
 
     const end_datetime = new Date();
@@ -18,6 +13,9 @@ describe('rabook app', () => {
     cy.get('[data-testid="calendar_datepicker"]').should('have.length', 2);
     cy.get('[data-testid="calendar_datepicker"]').last().type(end_datetime.toISOString()); // set the end calendar to an hour hence
 
+  })
+
+  it('Reserve "Northanger Abbey"', () => {
     cy.get('[data-testid="item"]').should('have.length', 4);
     cy.get('[data-testid="item"]').first().should('have.text', 'Northanger Abbey');
     cy.get('[data-testid="items_select"]').select('Northanger Abbey', {force: true});
@@ -30,15 +28,6 @@ describe('rabook app', () => {
   })
 
   it('Reserve "Persuasion"', () => {
-    cy.visit('https://10.0.2.15:5176/');
-
-    const end_datetime = new Date();
-    const current_hours = end_datetime.getHours();
-    end_datetime.setHours(current_hours + 1);
-
-    cy.get('[data-testid="calendar_datepicker"]').should('have.length', 2);
-    cy.get('[data-testid="calendar_datepicker"]').last().type(end_datetime.toISOString()); // set the end calendar to an hour hence
-
     cy.get('[data-testid="item"]').should('have.length', 3);
     cy.get('[data-testid="item"]').first().should('have.text', 'Persuasion');
     cy.get('[data-testid="items_select"]').select('Persuasion', {force: true});
@@ -51,15 +40,6 @@ describe('rabook app', () => {
   })
 
   it('Reserve "Pride and Prejudice"', () => {
-    cy.visit('https://10.0.2.15:5176/');
-
-    const end_datetime = new Date();
-    const current_hours = end_datetime.getHours();
-    end_datetime.setHours(current_hours + 1);
-
-    cy.get('[data-testid="calendar_datepicker"]').should('have.length', 2);
-    cy.get('[data-testid="calendar_datepicker"]').last().type(end_datetime.toISOString()); // set the end calendar to an hour hence
-
     cy.get('[data-testid="item"]').should('have.length', 2);
     cy.get('[data-testid="item"]').first().should('have.text', 'Pride and Prejudice');
     cy.get('[data-testid="items_select"]').select('Pride and Prejudice', {force: true});
@@ -72,15 +52,6 @@ describe('rabook app', () => {
   })
 
   it('Reserve "Sense and Sensibility"', () => {
-    cy.visit('https://10.0.2.15:5176/');
-
-    const end_datetime = new Date();
-    const current_hours = end_datetime.getHours();
-    end_datetime.setHours(current_hours + 1);
-
-    cy.get('[data-testid="calendar_datepicker"]').should('have.length', 2);
-    cy.get('[data-testid="calendar_datepicker"]').last().type(end_datetime.toISOString()); // set the end calendar to an hour hence
-
     cy.get('[data-testid="item"]').should('have.length', 1);
     cy.get('[data-testid="item"]').first().should('have.text', 'Sense and Sensibility');
     cy.get('[data-testid="items_select"]').select('Sense and Sensibility', {force: true});
