1c2bf85165454e9da636a62a8a23024a2e19415d
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-04-29 12:38:08 +0100

    start to pull form.tsx to pieces so we can unit test the bits

---
+++ src/components/get_non_empty_bucket.ts
@@ -0,0 +1,16 @@
+export const get_non_empty_bucket = (bucket: number, items_available: boolean []) => {
+  if (items_available[bucket]) {
+    console.log(`in get_non_empty_bucket and bucket is ${bucket}`);
+    return bucket;
+  }
+
+//      const next = (bucket + 1) % items_available.length;
+// or just start at 0 and return the first available ...
+  for (let i = 0; i < items_available.length; i++) {
+    if (items_available[i]) {
+      return i;
+    }
+  }
+};
+
+export default get_non_empty_bucket;
