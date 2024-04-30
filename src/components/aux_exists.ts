f859263f658d9338f3c109200f38931708bb993e
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-04-30 11:31:00 +0100

    return false when given an empty auxdata list

--- src/components/aux_exists.ts
+++ src/components/aux_exists.ts
@@ -1,7 +1,7 @@
 import { AuxDataRecordType } from './interfaces';
 
 export const aux_exists = (id: string, auxdata: AuxDataRecordType[]) => {
-  if (typeof auxdata === 'undefined') {
+  if (typeof auxdata === 'undefined' || !auxdata.length) {
     return false;
   } else {
     return auxdata.find(o => o.id === id);
