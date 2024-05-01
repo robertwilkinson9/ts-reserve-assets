a254a9377fd216d615f54644aeb0938d09e74c9c
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-05-01 13:12:29 +0100

    drop commented code

--- src/components/onChangeFn.ts
+++ src/components/onChangeFn.ts
@@ -2,22 +2,12 @@ import { AuxDataRecordType } from './interfaces';
 import { get_matching_aux_record } from './get_matching_aux_record';
 
 export const onChangeFn = (evalue: string, id: string, auxdata: AuxDataRecordType[], set_auxdata: React.Dispatch<React.SetStateAction<AuxDataRecordType[]>>) => {
-  console.log("onChangeFn");
-  console.log("auxdata");
-  console.dir(auxdata);
   const matching_record = get_matching_aux_record(id, auxdata);
-  console.log("onChangeFn");
-  console.log("matching_record");
-  console.dir(matching_record);
   if (matching_record === undefined) {
     const new_list = [ ...auxdata, {id: id, value: evalue }];
-    console.log("onChangeFn");
-    console.log("new_list");
-    console.dir(new_list);
     set_auxdata(new_list);
     return {};
   } else {
-    console.log("XXX onChangeFn");
     set_auxdata(
       auxdata.map((auxdata) =>
         auxdata.id === id
@@ -25,9 +15,6 @@ export const onChangeFn = (evalue: string, id: string, auxdata: AuxDataRecordTyp
         : { ...auxdata }
       )
     );
-    console.log("onChangeFn");
-    console.log("matching_record");
-    console.dir(matching_record);
     return matching_record;
   }
 };
