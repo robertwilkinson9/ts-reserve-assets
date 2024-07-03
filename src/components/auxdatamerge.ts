e3d7771d874601535a8a677b38cf0d24e316feb4
Robert Wilkinson <bob@fourtheye.org>
2024-07-03 16:44:11 +0000

    lint complained

--- src/components/auxdatamerge.ts
+++ src/components/auxdatamerge.ts
@@ -4,7 +4,7 @@ export const auxdatamerge = (aux_config: AuxConfigRecordType[], aux_data: AuxDat
     const data = aux_data.filter((d) => {
       if (d.id == c.id) {
         return d;
-      };
+      }
     });
     if (data && data[0] && data[0].value) {
       return {id: c.id, label: c.label, dbname: c.dbname, value: data[0].value};
